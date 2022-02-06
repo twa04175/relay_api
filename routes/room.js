c550b39003e4f332d0a6e7433bf5e3291bab7861
gimgyeongho <twa04175@naver.com>
2022-02-06 18:08:04 +0900

    /room/get?rid=n implements

--- routes/room.js
+++ routes/room.js
@@ -1,9 +1,19 @@
 var express = require('express');
 var router = express.Router();
+const db = require("../module/dbmodule");
 
-/* GET home page. */
+/* GET RoomInfo page. */
 router.get('/get', function(req, res, next) {
-    res.render('index', { title: 'Express' });
+    let query = "select * from relay.room_info where rid = ?;";
+    const rid = [req.query.rid];
+
+    db((err,connection) =>{
+        connection.query(query,rid,(error, rows)=>{
+            if(error) throw error;
+            res.send(rows);
+        });
+    })
 });
+
 
 module.exports = router;
